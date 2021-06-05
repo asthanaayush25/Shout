b705931030b768c061bafc22b507f7d1280d35fb
asthanaayush25 <asthanaayush25@gmail.com>
2021-06-05 16:37:40 +0530

    version 0.0.2
    built upon the previous version to make interaction better and easily possible

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const bodyParser=require('body-parser');
 const app = express();
 const http = require('http');
 const ngrok=require('ngrok');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+app.set("view engine","ejs");
+app.use(express.static('Public'));
+app.use(bodyParser.urlencoded({extended:true}));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.render('landing',{});
+});
+app.post('/three',(req,res)=>{
+  var user=req.body.user;
+  res.render('index',{
+    user : user
+  });
+});
+
+app.post('/one',(req,res)=>{
+var url=req.body.url;
+
+res.redirect(url.toString());
 });
 
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    socket.broadcast.emit('chat message', msg);
   });
 });
 const port=3000;
